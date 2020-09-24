c162daa63085185e881a6a15d0995ff8dd094d1e
BenyaminPCh18 <71095087+BenyaminPCh18@users.noreply.github.com>
2020-09-24 16:04:27 -0500

    grov

---
+++ js/index1.js
@@ -0,0 +1,24 @@
+
+$(document).ready(function(){
+  //ponemos nuestro codigo js. que utiliza JQuery
+  configurarInicio();
+  $(".iconito").click(function(e){
+    $('#navbar ul').toggleClass('lista-sm');
+  })
+  function configurarInicio(){
+    var urlPath=window.location.pathname;
+    console.log(urlPath);
+    $("nav a").each(function(){
+      var href=$(this).attr('href');
+      console.log(href);
+
+      var indice=urlPath.length - href.length;
+      console.log(indice);
+      console.log(urlPath.substring(indice));
+      if (urlPath.substring(indice)==href) {
+        // console.log('match');
+        $(this).closest('li').addClass('active');
+      }
+    })
+  }
+})
